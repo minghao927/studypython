bd8014311c130d36f4c930d4bbc020b9d4adfccb
XiaoRu <40014203+minghao927@users.noreply.github.com>
2019-03-13 11:11:28 +0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,51 @@
+# # # #
+# # # # age = int(input("请输出你家猫猫的年龄"))
+# # # # print("")
+# # # # if age <0:
+# # # #     print("不足一岁")
+# # # # elif age ==1:
+# # # #     print("相当于10岁的人")
+# # # # elif age ==2:
+# # # #     print("相当于20岁的人")
+# # # # elif age >2:
+# # # #     human = 20 + (age -2)*5
+# # # #     print("对应人类年龄：",human)
+# # # # input("点击退出")
+# # # #
+# # # # print(6==6)
+# # # # x=5
+# # # # y=8
+# # # # print(x==y)
+# # # number=7
+# # # guess=-1
+# # # print("猜数字")
+# # # while guess != number:
+# # #     guess = int(input("请输入"))
+# # #     if guess == number:
+# # #         print("猜对")
+# # #     elif guess < number:
+# # #         print("猜小了")
+# # #     elif guess > number:
+# # #         print("猜大了")
+# #
+# #
+# # n=int(input("请输入N的值"))
+# #
+# # sum=0
+# # counter=1
+# # while counter <=n:
+# #     sum=sum+counter
+# #     counter+=1
+# # print("1到%d 之和为：%d" %(n,sum))
+# #
+# count=0
+# while count<5:
+#     print(count,"<5")
+#     count = count +1
+# else:
+#     print(count,">=5")
+#
+flag=1
+while (flag):print('小茹欢迎你！！')
+print("再见！！")
+
