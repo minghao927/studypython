d96410b035ead58c610e907777a949141cb56e24
XiaoRu <40014203+minghao927@users.noreply.github.com>
2019-03-14 16:41:02 +0800

    xiaoru

--- 1.py
+++ 1.py
@@ -118,3 +118,19 @@
 # func(name='jack',age=30)
 # a=map(lambda x:x*2,[1,2,3])
 # list(a)
+# class mynumbers:
+#     def __iter__(self):
+#         self.a=1
+#         return self
+#     def __next__(self):
+#         x=self.a
+#         self.a+=1
+#         return x
+# myclass=mynumbers()
+# myiter=iter(myclass)
+#
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
+# print(next(myiter))
