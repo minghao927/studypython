b27fe2196aea1b8ef13a34e102aee27ef6108a80
XiaoRu <40014203+minghao927@users.noreply.github.com>
2019-03-15 15:31:19 +0800

    xiaoru

--- 1.py
+++ 1.py
@@ -293,3 +293,19 @@
 # test = sample("XiaoRu", 25, 80, 4, "Python")
 # y=test.speak()
 # print(y)
+
+class Parent:  # 定义父类
+    def myMethod(self):
+        print('调用父类方法')
+
+
+class Child(Parent):  # 定义子类
+    def myMethod(self):
+        print('调用子类方法')
+
+
+c = Child()  # 子类实例
+c.myMethod()  # 子类调用重写方法
+super(Child, c).myMethod()  # 用子类对象调用父类已被覆盖的方法
+
+
